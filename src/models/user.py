ba80b90db38be85a44167d96c7d30d1d464df173
josemorenoc <josemanuel.morenoca@gmail.com>
2024-02-18 12:52:56 -0500

    Revert "inclusión de id_user como Int, en clase User, y en la DB, para tener llave unica similar a Task... y mas escalable"
    This reverts commit a9ecf78abe31828abe192cbad5f3ded74099c810.

--- src/models/user.py
+++ src/models/user.py
@@ -5,10 +5,7 @@ from src.config.db_config import Base
 class User(Base):
     __tablename__ = 'users'
 
-    id_user = Column(Integer, primary_key=True, index=True, autoincrement=True)
-    # para dejar el id_usuario como llave única y escalar con indice númerico, como TASK
-
-    email = Column(String,unique=True)
+    email = Column(String, primary_key=True)
     username = Column(String, unique=True)
     password = Column(String)
 
