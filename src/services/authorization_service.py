f7823407ac017e2c2c604dcc2074dbbb9588fcbb
Cata1406 <m.ibanez@uniandes.edu.co>
2024-02-21 03:27:19 -0500

    Implement the authorization service

---
+++ src/services/authorization_service.py
@@ -0,0 +1,9 @@
+from fastapi_jwt_auth import AuthJWT
+from fastapi import HTTPException
+from sqlalchemy.orm import Session
+
+def authorized_user_email(Authorize: AuthJWT, user_email: str) -> str:
+    print(Authorize.get_jwt_subject())
+    authorized_user_email = Authorize.get_jwt_subject()
+    if authorized_user_email != user_email:
+        raise HTTPException(status_code=401, detail="Unauthorized access")
