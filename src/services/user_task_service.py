3acaa784cd96a4af831dabdeaca5a3549cd867f7
Cata1406 <m.ibanez@uniandes.edu.co>
2024-02-21 03:14:45 -0500

    Add user authentication to get_tasks_by_user_email

--- src/services/user_task_service.py
+++ src/services/user_task_service.py
@@ -1,10 +1,11 @@
 from sqlalchemy.orm import Session
 from src.services.user_service import get_user_by_email
 from src.models.user import User
-from src.models.task import Task
-from typing import List
+from fastapi_jwt_auth import AuthJWT
 
 
-def get_tasks_by_user_email(db: Session, email: str) -> list:
+
+def get_tasks_by_user_email(db: Session, Authorize: AuthJWT) -> list:
+    email = Authorize.get_jwt_subject()
     user: User = get_user_by_email(db, email)
     return user.tasks
